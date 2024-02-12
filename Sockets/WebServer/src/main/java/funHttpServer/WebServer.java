9721ca776d2fbb79ab0b9a8637ef33c8b4c9cbe2
John G <johngarvey1989@gmail.com>
2024-02-11 19:40:53 -0700

    update funWebServer

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -28,7 +28,7 @@ import java.nio.charset.Charset;
 
 class WebServer {
   public static void main(String args[]) {
-    WebServer server = new WebServer(9000);
+    WebServer server = new WebServer(8000);
   }
 
   /**
@@ -201,21 +201,44 @@ class WebServer {
           // extract path parameters
           query_pairs = splitQuery(request.replace("multiply?", ""));
 
+
           // extract required fields from parameters
-          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+          try{
+            Integer num1 = Integer.parseInt(query_pairs.get("num1"));
+            Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+            // do math
+            Integer result = num1 * num2;
+            // Generate response
+            builder.append("HTTP/1.1 200 OK\n");
+            builder.append("Content-Type: text/html; charset=utf-8\n");
+            builder.append("\n");
+            builder.append("Result is: " + result);
+          } catch (Exception e) {
+            e.printStackTrace();
+            response = ("<html>ERROR: " + e.getMessage() + "</html>").getBytes();
+            builder.append("HTTP/1.1 404 Not Found\n");
+            builder.append("Content-Type: text/html; charset=utf-8\n");
+            builder.append("\n");
+            builder.append("Failed to Multiply: "+ response);
+          }
 
-          // do math
-          Integer result = num1 * num2;
-
-          // Generate response
-          builder.append("HTTP/1.1 200 OK\n");
-          builder.append("Content-Type: text/html; charset=utf-8\n");
-          builder.append("\n");
-          builder.append("Result is: " + result);
-
-          // TODO: Include error handling here with a correct error code and
-          // a response that makes sense
+          // // Generate response
+          // if (true) { // success
+          //   // do math
+          //   Integer result = num1 * num2;
+          //   // Generate response
+          //   builder.append("HTTP/1.1 200 OK\n");
+          //   builder.append("Content-Type: text/html; charset=utf-8\n");
+          //   builder.append("\n");
+          //   builder.append("Result is: " + result);
+          // // TODO: Include error handling here with a correct error code and
+          // // a response that makes sense
+          // } else { // failure
+          //   builder.append("HTTP/1.1 404 Not Found\n");
+          //   builder.append("Content-Type: text/html; charset=utf-8\n");
+          //   builder.append("\n");
+          //   builder.append("Failed to Multiply");
+          // }
 
         } else if (request.contains("github?")) {
           // pulls the query from the request and runs it with GitHub's REST API
